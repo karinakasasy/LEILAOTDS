c2445c7cb8df01bdf16d1fd059afc3743688917c
karinakasasy <kasasy@gmail.com>
2024-07-15 17:18:00 -0300

    Configurando a conexão com o banco de dados

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -2,32 +2,25 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
-    public Connection connectDB(){
+
+    private static final String url = "jdbc:mysql://localhost:3306/leiloes";
+    private static final String user = "root";
+    private static final String senha = "465416";
+
+    public Connection connectDB() {
         Connection conn = null;
-        
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            conn = DriverManager.getConnection(url, user, senha);
+
+        } catch (SQLException e) {
+            System.out.println("Falha na conexaoDao " + e.getMessage());
+
         }
         return conn;
     }
-    
+
 }
