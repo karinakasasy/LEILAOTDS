593f657cad49e5e6adac22204f2b5f3b8a04f107
karinakasasy <kasasy@gmail.com>
2024-07-15 18:02:27 -0300

    Cadastro de produto no banco de dados e retorno da listagem em tela para o usuário

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,8 +12,8 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-
 
 public class ProdutosDAO {
     
@@ -24,19 +24,54 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        String sql = "INSERT INTO produtos (nome, valor, status) values (?,?,?)";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());            
+            prep.setDouble(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            prep.executeUpdate();
+            prep.close();
+
+            JOptionPane.showMessageDialog(null, "Produto inserido com sucesso");
+                                
+           } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro na ProdutosDAO " + e.getMessage());
+        }
     }
-    
+        
     public ArrayList<ProdutosDTO> listarProdutos(){
         
+        String sql = "SELECT * FROM produtos;";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO prodDto = new ProdutosDTO();
+
+                prodDto.setId(resultset.getInt("id"));
+                prodDto.setNome(resultset.getString("nome"));
+                prodDto.setValor(resultset.getInt("valor"));
+                prodDto.setStatus(resultset.getString("status"));
+                
+                listagem.add(prodDto);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ListarDao" + e.getMessage());
+
+        }
         return listagem;
     }
     
     
-    
-        
 }
 
